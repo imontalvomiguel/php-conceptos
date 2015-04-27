cd7a8e5fbe2af4bf14d3dc7e18ce9267587042aa
imontalvomiguel <ubaldo.im@gmail.com>
2015-04-26 19:31:04 -0500

    Errores en php

--- helloworld.php
+++ helloworld.php
@@ -3,8 +3,16 @@
  * Lógica de programación
  */
 
+  /**
+   * Errores en php
+   * Fatales.- Terminan la ejecución de la aplicación
+   * Warnings.- Cuando un tratas de incluir algo que no existe, etc
+   * Notice.- Error no tan importante, como cuando usas una variable no definida
+   */
+
   // Mostrar errores, estamos en desarrollo
-  ini_set('display_errors', 'on');
+  ini_set('display_errors', 'on'); // En el php.ini se establecen los settings, modificable en tiempo real
+  error_reporting(E_ALL); // Php... quiero ver todos los errores
 
   // Variables de la conexión a la base de datos
   $dsn = 'mysql:host=localhost;dbname=sakila';
