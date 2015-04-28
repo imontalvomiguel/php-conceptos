c6e550435f84b79f3b7b54fd2e5f3ef3e8a3e2d3
imontalvomiguel <ubaldo.im@gmail.com>
2015-04-27 21:43:08 -0500

    Filtrando la solicitud para la consulta de un film por id

--- films.php
+++ films.php
@@ -4,28 +4,34 @@
   require_once('database.php');
 
   if (!empty($_GET['id'])) {
-    $id = intval($_GET['id']);
+    $film_id = intval($_GET['id']);
+
+    // Título de la página
+    $title = 'Films';
+
+    // Para realizar consultar con el objeto PDO, tambíen manejaremos las Exceptions
+    try {
+      // Prepara la sonsulta SQL para ser ejecucata, utilizamos un placeholder ?
+      $results = $db->prepare('SELECT * FROM film WHERE film_id = ?');
+
+      // Asignamos los parámetros de la constulta con el método bindParam
+      $results->bindParam(1, $film_id);
+
+      // Ejecutar la consulta
+     $results->execute();
+
+      /**
+        * El objeto tiene el método fetchAll para retornar un arreglo con los resultados,
+        * el argumento le indica que quiero un array asociativo.
+       */
+      $film = $results->fetch(PDO::FETCH_ASSOC);
+
+    } catch(Exception $e) {
+      echo $e->getMessage();
+      die();
+    }
+
+    view('films', compact('film', 'title'));
   } else {
-    $id = null;
+    exit('Nothing found');
   }
-
-  // Título de la página
-  $title = 'Films';
-
-  // Para realizar consultar con el objeto PDO, tambíen manejaremos las Exceptions
-  try {
-    // Hacemos nuestra consulta... Nos regresa un objeto tipo PDOStatement
-    $results = $db->query('SELECT * FROM film WHERE film_id = ' . $id );
-
-    /**
-      * El objeto tiene el método fetchAll para retornar un arreglo con los resultados,
-      * el argumento le indica que quiero un array asociativo.
-     */
-    $film = $results->fetch(PDO::FETCH_ASSOC);
-
-  } catch(Exception $e) {
-    echo $e->getMessage();
-    die();
-  }
-
-  view('films', compact('film', 'title'));
